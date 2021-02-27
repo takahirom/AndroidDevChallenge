ebee0e53eee00d482a4ea6729229f3a9f1bb19a1
takahirom <takam.dev@gmail.com>
2021-02-27 12:33:58 +0900

    Update design and introduce navigation

--- week1/src/main/java/com/github/takahirom/androiddevchallenge/MainActivity.kt
+++ week1/src/main/java/com/github/takahirom/androiddevchallenge/MainActivity.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import com.github.takahirom.androiddevchallenge.ui.theme.AndroidDevChallengeTheme
-
-sealed class Screen {
-    object List : Screen()
-    class Detail(val index: Int) : Screen()
-}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,20 +27,31 @@ class MainActivity : AppCompatActivity() {
         setContent {
             AndroidDevChallengeTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    val screenState = remember { mutableStateOf<Screen>(Screen.List) }
-                    when (val screen = screenState.value) {
-                        Screen.List ->
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = "list") {
+                        composable(
+                            route = "list",
+                        ) {
                             PuppyList {
-                                screenState.value = Screen.Detail(it)
+                                navController.navigate("detail/$it")
                             }
-                        is Screen.Detail -> {
+                        }
+                        composable(
+                            route = "detail/{index}",
+                            arguments = listOf(
+                                navArgument("index") {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) { backStackEntry ->
+                            val index = backStackEntry
+                                .arguments?.getString("index")
+                                ?.toInt() ?: throw IllegalArgumentException()
                             PuppyDetail(
-                                screen.index
+                                index
                             )
                         }
-
                     }
-
                 }
             }
         }
@@ -51,22 +61,41 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-class Puppy(val name: String, val age: Int)
+class Puppy(val image: String, val name: String, val age: Int)
 
 val puppis = listOf(
-    Puppy("Abby", 10),
-    Puppy("Abe", 5),
-    Puppy("Addie", 5)
+    Puppy("🐕", "Abby", 10),
+    Puppy("🐶", "Abe", 5),
+    Puppy("🐩", "Addie", 5)
 )
 
 @Composable
 fun PuppyList(onClick: (Int) -> Unit) {
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
         items(puppis) { puppy ->
-            Text(
-                modifier = Modifier.clickable { onClick(puppis.indexOf(puppy)) },
-                text = puppy.name
-            )
+            Card(
+                modifier = Modifier
+                    .clickable { onClick(puppis.indexOf(puppy)) }
+                    .padding(4.dp)
+                    .fillMaxWidth(),
+                elevation = 4.dp
+            ) {
+                Row {
+                    Text(
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .size(30.dp),
+                        text = puppy.image,
+                        style = TextStyle(fontSize = 24.sp)
+                    )
+                    Text(
+                        modifier = Modifier,
+                        text = puppy.name,
+                        style = MaterialTheme.typography.h4
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(4.dp))
         }
     }
 }
@@ -75,8 +104,23 @@ fun PuppyList(onClick: (Int) -> Unit) {
 private fun PuppyDetail(index: Int) {
     val puppy = puppis[index]
     Column {
-        Text("name:" + puppy.name)
-        Text("age:" + puppy.age.toString())
+        Text(
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            text = puppy.image,
+            style = TextStyle(fontSize = 80.sp)
+        )
+        Text(
+            modifier = Modifier
+                .padding(16.dp),
+            text = "name:" + puppy.name
+        )
+        Text(
+            modifier = Modifier
+                .padding(16.dp),
+            text = "age:" + puppy.age.toString()
+        )
     }
 }
 
